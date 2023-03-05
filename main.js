1d622133ca2df37c613f3b90e37c7d99220cf1e0
stapler-codes <123899436+stapler-codes@users.noreply.github.com>
2023-03-05 09:56:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var score = 0;
+document.getElementById("question1_option1").addEventListener('click', function() {
+    score += 0;
+    document.getElementById("question1").style.visibility = "hidden";
+    document.getElementById("question2").style.visibility = "visible";
+    alert(score)
+})
+document.getElementById("question1_option2").addEventListener('click', function() {
+    score += 1;
+    document.getElementById("question1").style.visibility = "hidden";
+    alert(score)
+})
